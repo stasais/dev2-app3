c3ecb4e051fc19fce287d536398943ee6a7f51cb
stasais <s2410929003@students.fh-hagenberg.at>
2025-06-05 13:27:51 +0200

    add k8s resourses and OTEL setup upd

--- src/dev2il_devops_app/main.py
+++ src/dev2il_devops_app/main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from dev2il_devops_app import setup_otel
+from dev2il_devops_app.otel import setup_otel
 
 app = FastAPI(title='dev2il-devops-app')
 data_file_path = 'data.csv'
